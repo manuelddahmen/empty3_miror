dc98fb4c3325d020145338ff3359cd79a15a243d
Manuel Dahmen <dathewolf@gmail.com>
2022-05-15 11:16:50 +0000

    Jobs

--- .space.kts
+++ .space.kts
@@ -3,39 +3,6 @@
 * This Kotlin-script file lets you automate build activities
 * For more info, refer to https:*/
 
-plugins {
-    `java-library`
-}
-
-dependencies {                              
-    api("junit:junit:4.13")
-    implementation("junit:junit:4.13")
-    testImplementation("junit:junit:4.13")
-}
-
-configurations {                            
-    implementation {
-        resolutionStrategy.failOnVersionConflict()
-    }
-}
-
-sourceSets {                                
-    main {                                  
-        java.srcDir("src/core/java")
-    }
-}
-
-java {                                      
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
-tasks {
-    test {                                  
-        testLogging.showExceptions = true
-    }
-}
-
 job("Build and publish") {
     container(displayName = "Run publish script", image = "gradle") {
         kotlinScript { api ->
