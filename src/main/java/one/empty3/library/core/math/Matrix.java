a20afef9e0179b8d308d063b5f2a3f4b9b41607d
dathewolf <manuel.dahmen@gmx.com>
2022-06-30 22:20:29 +0200

    Réseau de neurones

--- src/main/java/one/empty3/library/core/math/Matrix.java
+++ src/main/java/one/empty3/library/core/math/Matrix.java
@@ -3,11 +3,12 @@ package one.empty3.library.core.math;
 import java.util.Arrays;
 
 public class Matrix {
-    private static final String NUMBER_FORMAT = "%+12.5f";
+    public static final double DOUBLE_MIN = 0.0001;
+    public static final String NUMBER_FORMAT = "%+12.5f";
     private static final double TOLERANCE = 0.000001;
     private final int cols;
     private final int rows;
-    private double[] a;
+    private final double[] a;
 
     public Matrix(int rows, int cols) {
         this.rows = rows;
@@ -54,7 +55,7 @@ public class Matrix {
     public Matrix multiply(Matrix m) {
         Matrix result = new Matrix(rows, m.cols);
 
-        assert cols == m.rows : "Cannot multipy";
+        assert cols == m.rows : "Cannot multiply";
         int index = 0;
         for (int row = 0; row < result.rows; row++) {
             for (int col = 0; col < result.cols; col++) {
@@ -99,7 +100,29 @@ public class Matrix {
     public void set(int row, int col, double value) {
         a[row*cols+col] = value;
     }
+    public void set(int row, double value) {
+        a[row] = value;
+    }
 
+    public double get(int index) {
+        return a[index];
+    }
+
+    public Matrix softmax() {
+        Matrix matrix = new Matrix(rows, cols);
+        double sumExp = 0.0;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                sumExp += Math.exp(get(i,j));
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix.set(i, j, Math.exp(get(i,j))/sumExp);
+            }
+        }
+        return matrix;
+    }
 
     public interface Producer {
         double produce(int index);
@@ -122,4 +145,46 @@ public class Matrix {
         }
         return this;
     }
+
+
+    public Matrix modify(ValueProducer producer) {
+        for (int i = 0; i < a.length; i++) {
+            a[i] = producer.produce(i, a[i]);
+        }
+        return this;
+    }
+
+    public interface IndexValueConsumer {
+        public void consume(int index, double value);
+    }
+    public void forEach(IndexValueConsumer consumer) {
+        for (int i = 0; i < a.length; i++) {
+            consumer.consume(i, a[i]);
+        }
+    }
+    public interface RowColIndexValueConsumer {
+        public void consume(int row, int col, int index, double value);
+    }
+    public void forEach(RowColIndexValueConsumer consumer) {
+        int index = 0;
+            for (int row = 0; row < rows; row++) {
+                for (int col = 0; col < cols; col++) {
+                    consumer.consume(row, col, index, get(row, col));
+                    index++;
+                }
+            }
+
+    }
+    public Matrix sumColumns() {
+        Matrix matrix = new Matrix(1, cols);
+        forEach((row, col, index, value) -> matrix.set(0, col, matrix.get(0, col)+value));
+        return matrix;
+    }
+    public Matrix sumRows() {
+        Matrix matrix = new Matrix(rows, 1);
+        forEach((row, col, index, value) -> matrix.set(row, 0, matrix.get(row, 0)+value));
+        return matrix;
+    }
+
+
 }
--- src/test/java/one/empty3/neunet/TestNeunet.java
+++ src/test/java/one/empty3/neunet/TestNeunet.java
@@ -1,56 +1,135 @@
 package one.empty3.neunet;
 
 import one.empty3.library.core.math.Matrix;
-import one.empty3.neunet.ActivationFunction;
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicReference;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestNeunet {
+    private static Random random;
+
+    @Test
+    public void testCrossEntropy() {
+        double[] expectedValues = new double[]{1, 0, 0, 0, 0, 1, 0, 1, 0};
+        Matrix expected = new Matrix(3, 3, i -> expectedValues[i]);
+
+        System.out.println(expected);
+
+        Matrix actual = new Matrix(3, 3, i -> 2 * i - 4).softmax();
+
+        System.out.println(actual);
+
+        Matrix result = LossFunction.crossEntropy(expected, actual);
+
+        System.out.println(result);
+
+        actual.forEach(((row, col, index, value) -> {
+            double expectedValue = value;
+            if (expectedValue >= 0.9) {
+
+            }
+        }));
+
+    }
+
     @Test
     public void testFunctionAnd() {
         Net<Neuron> net = new Net<>();
         net.setInputLayer(new Layer<Neuron>(2, Neuron.class));
         net.getInputLayer().getNeurons().setElem(new Neuron(2), 0);
 
-        net.getInputLayer().getNeurons().getElem(0).setW(new double[] {0.5, 0.5});
+        net.getInputLayer().getNeurons().getElem(0).setW(new double[]{0.5, 0.5});
 
         net.getInputLayer().getNeurons().getElem(0).setActivationMethod(ActivationMethod.MinMax01);
 
-        net.getInputLayer().getNeurons().getElem(0).setInput(new double[] {0.,0.});
+        net.getInputLayer().getNeurons().getElem(0).setInput(new double[]{0., 0.});
         net.computeAll();
         assertEquals(0.0, net.getInputLayer().getNeurons().getElem(0).getOutput());
-        net.getInputLayer().getNeurons().getElem(0).setInput(new double[] {0.,1.});
-        net.computeAll();
-        assertEquals(0.0, net.getInputLayer().getNeurons().getElem(0).getOutput());
-
-        net.getInputLayer().getNeurons().getElem(0).setInput(new double[] {1.,0.});
+        net.getInputLayer().getNeurons().getElem(0).setInput(new double[]{0., 1.});
         net.computeAll();
         assertEquals(0.0, net.getInputLayer().getNeurons().getElem(0).getOutput());
 
-        net.getInputLayer().getNeurons().getElem(0).setInput(new double[] {1.,1.});
+        net.getInputLayer().getNeurons().getElem(0).setInput(new double[]{1., 0.});
+        net.computeAll();
+        assertEquals(0.0, net.getInputLayer().getNeurons().getElem(0).getOutput());
+
+        net.getInputLayer().getNeurons().getElem(0).setInput(new double[]{1., 1.});
         net.computeAll();
         assertEquals(1.0, net.getInputLayer().getNeurons().getElem(0).getOutput());
 
         net.getInputLayer().getNeurons().getData1d().forEach(n -> System.out.println(n.getOutput()));
 
 
-
-
     }
+
     @Test
     public void testMatrices() {
-        Matrix inputs = new Matrix(3, 3);
-        Matrix weights = new Matrix(3, 3);
-        Matrix biases = new Matrix(3, 1);
+        random = new Random();
 
+        int numberOfInputs = 7;
+        int numberNeurons = 6;
+        int inputSize = 4;
+        Matrix inputs = new Matrix(inputSize, numberOfInputs);
+        Matrix weights = new Matrix(numberNeurons, inputSize);
+        Matrix biases = new Matrix(numberNeurons, 1);
 
-        Matrix modify = inputs.multiply(weights).modify((row, col, value) -> value + biases.get(row, 1));
+        inputs.modify((row, col, value) -> random.nextDouble());
+        weights.modify((row, col, value) -> random.nextGaussian());
+        biases.modify((row, col, value) -> random.nextGaussian());
+
+        Matrix result1 = weights.multiply(inputs).modify((row, col, value) -> value + biases.get(row));
+        Matrix result2 = weights.multiply(inputs).modify((row, col, value) -> value + biases.get(row))
+                .modify((index, value) -> value > 0 ? value : 0.0);
 
         System.out.println(inputs);
         System.out.println(weights);
         System.out.println(biases);
-        System.out.println(modify);
+        System.out.println(result1);
+        System.out.println(result2);
+
+        AtomicReference<Double> softmax = new AtomicReference<>(0.0);
+        result2.forEach((index, value) -> {
+            softmax.updateAndGet(v -> (double) (v + Math.exp(result2.get(index))));
+            //assertTrue(value-r1<Matrix.DOUBLE_MIN);
+        });
+        result2.modify((index, value) -> Math.exp(value) / softmax.get());
+        System.out.println(result2);
+    }
+
+    @Test
+    public void testMatrices1() {
+        random = new Random();
+
+        int numberOfInputs = 7;
+        int numberNeurons = 6;
+        int inputSize = 4;
+        Matrix inputs = new Matrix(inputSize, numberOfInputs);
+        Matrix weights = new Matrix(numberNeurons, inputSize);
+        Matrix biases = new Matrix(numberNeurons, 1);
+
+        inputs.modify((row, col, value) -> random.nextDouble());
+        weights.modify((row, col, value) -> random.nextGaussian());
+        biases.modify((row, col, value) -> random.nextGaussian());
+
+        Matrix result1 = weights.multiply(inputs).modify((row, col, value) -> value + biases.get(row));
+        Matrix result2 = weights.multiply(inputs).modify((row, col, value) -> value + biases.get(row))
+                .modify((index, value) -> value > 0 ? value : 0.0);
+
+        System.out.println(inputs);
+        System.out.println(weights);
+        System.out.println(biases);
+        System.out.println(result1);
+        System.out.println(result2);
+
+        AtomicReference<Double> softmax = new AtomicReference<>(0.0);
+        result2.forEach((index, value) -> {
+            softmax.updateAndGet(v -> (double) (v + Math.exp(result2.get(index))));
+            //assertTrue(value-r1<Matrix.DOUBLE_MIN);
+        });
+        result2.modify((index, value) -> Math.exp(value) / softmax.get());
+        System.out.println(result2);
     }
 }
