5a798a7f183211ab7964eec9d90c0a0acb4419b2
dathewolf <dathewolf@gmail.com>
2022-07-24 21:32:04 +0200

    mesh

--- src/main/java/one/empty3/modeilling/Face.java
+++ src/main/java/one/empty3/modeilling/Face.java
@@ -8,8 +8,11 @@ import one.empty3.library.Scene;
 import one.empty3.library.core.export.STLExport;
 import one.empty3.library.core.nurbs.SurfaceParametricPolygonalBezier;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 public class Face extends RepresentableConteneur {
     private Rectangle2 rectangle2 = new Rectangle2(-1, -1, 2, 2);
@@ -28,18 +31,18 @@ public class Face extends RepresentableConteneur {
                 {
                         -1, 0, 0.5, 0, -1, -1
                 }, {
-                0, 0.5, 0.7, 0.5, 0, -0.3
+                0.6, 0.6, 0.7, 0.5, 0.6, -0.6
         }, {
-                0, 0.4, 0.8, 0.4, 0, -0.1
+                0.6, 0.6, 0.8, 0.7, 0.6, -0.1
 
         }, {
-                0, 0.6, 0.7, 0.6, 0, 0
+                0.6, 0.6, 0.7, 0.6, 0.6, 0
 
         },{
-                0, 0.2, 0.3, 0.2, 0, 0.1
+                0.6, 0.7, 0.7, 0.7, 0, 0.1
 
         }, {
-                0, 0.6, 0.7, 0.6, 0, 0.12
+                0, 0.6, 0.7, 0.6, 0, 0.6
 
         },{
                 -1, -1, -1, -1, -1, 1
@@ -51,11 +54,11 @@ public class Face extends RepresentableConteneur {
             }
         }
         for (int i = 3; i < controls.length - 3; i++) {
-            for (int j = 3; j < controls[i].length - 3; j++) {
+            for (int j = 3; j < controls[i].length - 4; j++) {
                 double y = z[i][z[i].length-1];
                 controls[i][j].set(2, z[i - 3][j - 3]);
                 controls[i][j].set(1, y);
-
+                //controls[i][j].set(0, )
             }
         }
         SurfaceParametricPolygonalBezier s = new SurfaceParametricPolygonalBezier(controls);
@@ -63,10 +66,21 @@ public class Face extends RepresentableConteneur {
         Scene scene = new Scene();
 
         scene.add(s);
-
+        File file = new File("c:\\users\\manue\\Desktop\\me3D-" + Math.random() + ".stl");
         try {
-            STLExport.save(new File("c:\\users\\manue\\Desktop\\me3D.stl"), scene, false);
+            STLExport.save(file, scene, false);
+            {
+                Process process = new ProcessBuilder("explorer", file.getAbsolutePath(), "\"").start();
+                int errorCode = process.waitFor();
+// adjust the error code for your use case, different programs migth not use 0 as success
+                if (errorCode != 0) {
+                    try (BufferedReader reader = process.errorReader(StandardCharsets.UTF_8)) {
+                    }
+                }
+            }
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
 
