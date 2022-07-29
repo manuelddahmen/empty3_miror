6b1cfe3fc35a640bbac542732c95ff46de05904d
dathewolf <dathewolf@gmail.com>
2022-07-29 08:58:37 +0200

    mesh

--- build.cmd
+++ build.cmd
@@ -1,3 +1,4 @@
 set MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
-set JAVA_HOME="C:\Program Files\AdoptOpenJDK\jdk-16"
-"C:\Program Files\JetBrains\IntelliJ IDEA 2020.3.3\plugins\maven\lib\maven3\bin\mvn" deploy
+set JAVA_HOME="C:\Program Files\Java\jdk-11.0.13"
+set ANDROID_SDK_ROOT="C:\Users\manue\AppData\Local\Android\Sdk"
+"C:\Program Files\JetBrains\IntelliJ IDEA 2022.1.4\plugins\maven\lib\maven3\bin\mvn" jar
