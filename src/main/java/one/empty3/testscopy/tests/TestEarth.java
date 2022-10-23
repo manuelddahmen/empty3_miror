53ce0bdf99305bdd829d750c9cb7de0b89091d26
manuel dahmen <manuel.dahmen@gmx.com>
2022-10-23 18:20:28 +0200

    New game start development

--- src/main/java/one/empty3/testscopy/tests/TestEarth.java
+++ src/main/java/one/empty3/testscopy/tests/TestEarth.java
@@ -3,9 +3,7 @@ package one.empty3.testscopy.tests;
 import one.empty3.feature.app.replace.javax.imageio.ImageIO;
 import one.empty3.library.*;
 import one.empty3.library.core.nurbs.CameraInPath;
-import one.empty3.library.core.nurbs.ParametricCurve;
 import one.empty3.library.core.testing.TestObjet;
-import one.empty3.library.core.testing.TestObjetSub;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -14,7 +12,7 @@ import java.io.File;
 public class TestEarth extends TestObjet {
     private File planets = new File("res/img/planets");
     private File[] planetsImagesFiles;
-    private int i = 0;
+    private int i = -1;
     private BufferedImage image;
     private Sphere sphere;
 
@@ -25,6 +23,8 @@ public class TestEarth extends TestObjet {
         z().texture(new ColorTexture(Color.BLACK));
         scene().texture(new ColorTexture(Color.BLACK));
         scene().add(sphere);
+
+        incr();
     }
 
     @Override
@@ -37,26 +37,28 @@ public class TestEarth extends TestObjet {
 
     }
 
+    public void incr() {
+        i++;
+        if (i < planets.length())
+            image = ImageIO.read(planetsImagesFiles[i]);
+        TextureImg textureImg = new TextureImg(new ECBufferedImage(image));
+        sphere.texture(textureImg);
 
+    }
 
     @Override
     public void finit() throws Exception {
-        if((frame()/25/10)%planets.length()==0) {
-            i++;
-            if(i<planets.length())
-                image = ImageIO.read(planetsImagesFiles[i]);
-            TextureImg textureImg = new TextureImg(new ECBufferedImage(image));
-            sphere.texture(textureImg);
-
-
+        if ((frame() / 25 / 10) % planets.length() == 0) {
+            incr();
         }
-        Point3D p1axe = Point3D.Z.mult(-2 * Math.cos(2 * Math.PI * (frame() / 10.))).plus(
-                Point3D.X.mult(-2 * Math.sin(2 * Math.PI * (frame() / 10.))));
-        Circle circle = new Circle(new Axe(p1axe, p1axe.mult(-1)), 1.0);
+        //Point3D p1axe = Point3D.Z.mult(-2 * Math.cos(2 * Math.PI * (frame() / 10.))).plus(
+        //        Point3D.X.mult(-2 * Math.sin(2 * Math.PI * (frame() / 10.))));
+        Point3D p1axe = Point3D.Z;
+        Circle circle = new Circle(new Axe(p1axe.mult(10), p1axe.mult(-10)), 20.0);
 
         CameraInPath camera = new CameraInPath(circle);
 
-        camera.setT(1.0);
+        camera().calculerMatrice(Point3D.X);
 
         scene().cameraActive(camera);
     }
