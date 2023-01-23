2ad4353d9f5254161aa38aca0504f2e5d400a84f
manuel dahmen <manuel.dahmen@gmx.com>
2023-01-23 21:24:18 +0100

    open/save project Works FIXED: Grid saving/opening. Insert ROW/COL, oK Exception in Polygons.java Algorithm ImageControls preview déformation Amélioré.

--- src/main/java/one/empty3/library/Polygons.java
+++ src/main/java/one/empty3/library/Polygons.java
@@ -25,21 +25,21 @@ public class Polygons extends SurfaceParametriquePolynomiale {
     @Override
     public Point3D calculerPoint3D(double u, double v) {
         try {
-            int indexU0 = (int) (u * coefficients.getData2d().get(0).size());
-            int indexV0 = (int) (v * coefficients.getData2d().size());
-            if (indexU0 > coefficients.getData2d().get(0).size() - 1) {
-                indexU0 = coefficients.getData2d().get(0).size() - 1;
+            int indexU0 = (int) (u * coefficients.getData2d().size());
+            int indexV0 = (int) (v * coefficients.getData2d().get(0).size());
+            if (indexU0 > coefficients.getData2d().size() - 1) {
+                indexU0 = coefficients.getData2d().size() - 1;
             }
-            if (indexV0 > coefficients.getData2d().size() - 1) {
-                indexV0 = coefficients.getData2d().size() - 1;
+            if (indexV0 > coefficients.getData2d().get(0).size() - 1) {
+                indexV0 = coefficients.getData2d().get(0).size() - 1;
             }
             int indexU1 = (int) (indexU0 + 1.);
             int indexV1 = (int) (indexV0 + 1.);
-            if (indexU1 > coefficients.getData2d().get(0).size() - 1) {
-                indexU1 = coefficients.getData2d().get(0).size() - 1;
+            if (indexU1 > coefficients.getData2d().size() - 1) {
+                indexU1 = coefficients.getData2d().size() - 1;
             }
-            if (indexV1 > coefficients.getData2d().size() - 1) {
-                indexV1 = coefficients.getData2d().size() - 1;
+            if (indexV1 > coefficients.getData2d().get(0).size() - 1) {
+                indexV1 = coefficients.getData2d().get(0).size() - 1;
             }
             if(coefficients.inBounds(indexU0, indexV0) && coefficients.inBounds(indexU1, indexV0)
                 &&coefficients.inBounds(indexU1, indexV1) && coefficients.inBounds(indexU0, indexV1)) {
@@ -47,13 +47,13 @@ public class Polygons extends SurfaceParametriquePolynomiale {
                         coefficients.getElem(indexU0, indexV0), coefficients.getElem(indexU1, indexV0),
                         coefficients.getElem(indexU1, indexV1), coefficients.getElem(indexU0, indexV1)
                 };
-                double U = u * (coefficients.getData2d().get(0).size()) - indexU0;
-                double V = v * (coefficients.getData2d().size()) - indexV0;
+                double U = u * (coefficients.getData2d().size()) - indexU0;
+                double V = v * (coefficients.getData2d().get(0).size()) - indexV0;
                 assert U >= 0 && U <= 1 && V >= 0 && V <= 1;
                 Point3D pUv0 = points[0].plus(points[1].moins(points[0]).mult(U));
                 Point3D pUv1 = points[3].plus(points[2].moins(points[3]).mult(U));
                 Point3D pU0v = points[3].moins(points[0]).mult(V);
-                Point3D pU1v = points[3].moins(points[2]).mult(V);
+                Point3D pU1v = points[2].moins(points[1]).mult(V);
                 return pUv0.plus(pUv1.moins(pUv0).mult(V)); // Discutable
             }
         } catch (NullPointerException ex) {
--- src/main/java/one/empty3/library/PolygonsDistinctUV.java
+++ src/main/java/one/empty3/library/PolygonsDistinctUV.java
@@ -51,37 +51,37 @@ public class PolygonsDistinctUV extends Polygons {
             public int getColorAt(double u, double v) {
                 try {
 
-                    int indexU0 = (int) (u * uvMap.getData2d().get(0).size());
-                    int indexV0 = (int) (v * uvMap.getData2d().size());
-                    if (indexU0 > uvMap.getData2d().get(0).size() - 1) {
-                        indexU0 = uvMap.getData2d().get(0).size() - 1;
+                    int indexU0 = (int) (u * uvMap.getData2d().size());
+                    int indexV0 = (int) (v * uvMap.getData2d().get(0).size());
+                    if (indexU0 > uvMap.getData2d().size() - 1) {
+                        indexU0 = uvMap.getData2d().size() - 1;
                     }
-                    if (indexV0 > uvMap.getData2d().size() - 1) {
-                        indexV0 = uvMap.getData2d().size() - 1;
+                    if (indexV0 > uvMap.getData2d().get(0).size() - 1) {
+                        indexV0 = uvMap.getData2d().get(0).size() - 1;
                     }
                     int indexU1 = (int) (indexU0 + 1.);
                     int indexV1 = (int) (indexV0 + 1.);
-                    if (indexU1 > uvMap.getData2d().get(0).size() - 1) {
-                        indexU1 = uvMap.getData2d().get(0).size() - 1;
+                    if (indexU1 > uvMap.getData2d().size() - 1) {
+                        indexU1 = uvMap.getData2d().size() - 1;
                     }
-                    if (indexV1 > uvMap.getData2d().size() - 1) {
-                        indexV1 = uvMap.getData2d().size() - 1;
+                    if (indexV1 > uvMap.getData2d().get(0).size() - 1) {
+                        indexV1 = uvMap.getData2d().get(0).size() - 1;
                     }
                     if(coefficients.inBounds(indexU0, indexV0) && coefficients.inBounds(indexU1, indexV0)
                             &&coefficients.inBounds(indexU1, indexV1) && coefficients.inBounds(indexU0, indexV1)) {
                         Point3D[] uvQuad = new Point3D[]{
-                                uvMap.getElem(indexU0, indexV0), uvMap.getElem(indexU1, indexV0),
-                                uvMap.getElem(indexU1, indexV1), uvMap.getElem(indexU0, indexV1)
+                                uvMap.getElem(indexU0, indexV1), uvMap.getElem(indexU1, indexV1),
+                                uvMap.getElem(indexU1, indexV0), uvMap.getElem(indexU0, indexV0)
                         };
 
-                        double U = u * (uvMap.getData2d().get(0).size()) - indexU0;
-                        double V = v * (uvMap.getData2d().size()) - indexV0;
+                        double U = u * (uvMap.getData2d().size()) - indexU0;
+                        double V = v * (uvMap.getData2d().get(0).size()) - indexV0;
                         assert U >= 0 && U <= 1 && V >= 0 && V <= 1;
                         Point3D pUv0 = uvQuad[0].plus(uvQuad[1].moins(uvQuad[0]).mult(U));
                         Point3D pUv1 = uvQuad[3].plus(uvQuad[2].moins(uvQuad[3]).mult(U));
                         Point3D pU0v = uvQuad[3].moins(uvQuad[0]).mult(V);
-                        Point3D pU1v = uvQuad[3].moins(uvQuad[2]).mult(V);
-                        Point3D plus = pUv0.plus(pUv1.moins(pUv0).mult(V));// Discutable
+                        Point3D pU1v = uvQuad[2].moins(uvQuad[1]).mult(V);
+                        Point3D plus = pUv1.plus(pUv0.moins(pUv1).mult(V));// Discutable
                         return texture2.getColorAt(plus.getX(), plus.getY());
                     }
                 } catch(Exception ex) {
