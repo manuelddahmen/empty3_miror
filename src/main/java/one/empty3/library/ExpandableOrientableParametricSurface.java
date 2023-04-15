49ffaf388fa9adbfc609065bde8bb3ecc3e1177d
manuel dahmen <manuel.dahmen@gmx.com>
2023-04-15 07:07:18 +0200

    OrientableParametricSurface

--- src/main/java/one/empty3/library/ExpandableOrientableParametricSurface.java
+++ src/main/java/one/empty3/library/ExpandableOrientableParametricSurface.java
@@ -21,6 +21,7 @@ package one.empty3.library;
 
 import one.empty3.library.core.nurbs.ParametricSurface;
 
+
 public class ExpandableOrientableParametricSurface extends OrientableParametricSurface{
     public ExpandableOrientableParametricSurface(ParametricSurface ps) {
         super(ps);
