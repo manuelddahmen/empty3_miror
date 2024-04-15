3407cfd4fd6c42a0c6efe14432de57e1e60f17f3
Manuel Daniel Dahmen <manuel.dahmen@gmx.com>
2024-04-15 12:28:46 +0200

    TestStringAnalyzer5.kt - New features in incubation; tokenNew (construction definition, call) - new Array[] (declaration, construct, items' access)
    Acked-by: Manuel Dahmen <dathewolf@gmail.com>
    Signed-off-by: Manuel Daniel Dahmen <manuel.dahmen@gmx.com>

--- src/main/java/one/empty3/library1/tree/StringAnalyzerJava1.java
+++ src/main/java/one/empty3/library1/tree/StringAnalyzerJava1.java
@@ -157,7 +157,48 @@ public class StringAnalyzerJava1 extends StringAnalyzer3 {
 
     }
 
-    public class TokenName2 extends Token {
+    public class MultiTokenSequence extends Token {
+        private final Token start;
+        private final Token end;
+        private final Token separator;
+        private final Token tokenType;
+
+        public MultiTokenSequence(Token start, Token end, Token separator, Token tokenType) {
+            this.start = start;
+            this.end = end;
+            this.separator = separator;
+            this.tokenType = tokenType;
+        }
+
+        @Override
+        public int parse(String input, int position) {
+            if (position >= input.length() || input.substring(position).trim().isEmpty()) {
+                mPosition = position;
+                setSuccessful(false);
+                throw new RuntimeException(getClass() + " : position>=input.length()");
+            }
+
+            int position1 = start.parse(input, position);
+            if (start.isSuccessful()) {
+                while (((position1 = tokenType.parse(input, position1)) > position)) {
+                    if (tokenType.isSuccessful()) {
+                        position = position1;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            position1 = end.parse(input, position);
+            if (end.isSuccessful()) {
+                return processNext(input, position1);
+            } else {
+                return position;
+            }
+        }
+    }
+
+
+    public class TokenExpression2 extends Token {
         protected String name = null;
         private String[] brackets = new String[7];
         public boolean classVariable0;
@@ -174,7 +215,7 @@ public class StringAnalyzerJava1 extends StringAnalyzer3 {
         public boolean numberString;
         public boolean numberChar;
 
-        public TokenName2() {
+        public TokenExpression2() {
             super();
         }
 
@@ -214,15 +255,30 @@ public class StringAnalyzerJava1 extends StringAnalyzer3 {
                 i++;
             }
             boolean bStart = false;
+            boolean pStart = false;
             while (i < input.length() && (((Character.isWhitespace(input.charAt(i))) || (input.charAt(i) == '[') || (input.charAt(i) == ']')))) {
                 if (input.charAt(i) == '[' && !bStart) {
                     bStart = true;
                     i1[passBrackets] = i;
-                }
-                if (input.charAt(i) == ']' && bStart) {
+                } else if (input.charAt(i) == ']' && bStart) {
                     bStart = false;
                     i1[passBrackets + 1] = i;
                     passBrackets += 2;
+                } else if (bStart) {
+                    TokenExpression2 tokenName2 = new TokenExpression2();
+                    int parse = tokenName2.parse(input, i);
+                    if (tokenName2.isSuccessful()) {
+                        i = parse;
+                    }
+                }
+                if (input.charAt(i) == '(' && !pStart) {
+                    pStart = true;
+                } else if (input.charAt(i) == ')' && pStart) {
+                    MultiTokenSequence multiTokenSequence = new MultiTokenSequence(new TokenOpenParenthesized(), new TokenCloseParenthesized(),
+                            new TokenComa(), new TokenExpression2());
+                    int parse = multiTokenSequence.parse(input, i);
+                    i = parse;
+                    pStart = false;
                 }
                 i++;
             }
